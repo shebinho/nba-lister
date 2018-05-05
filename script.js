85ba7184a3b7d8f636749c3fa08246cabb510a9a
shebinho <33525543+shebinho@users.noreply.github.com>
2018-05-05 02:33:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,16 @@ $(document).ready(function () {
         // const filteredplayers = nba.filter(nba => nba.team[0].ATL.player)
         // $("<p>")
         for(let i = 0 ; i < nba.teams.length ; i++){
+            console.log("Team Name: "+ nba.teams[i].name)
             for(let j = 0; j < nba.teams[i].players.length; j++) {
-                console.log(nba.teams[i].players[j].name);
+                console.log("Player Name: "+nba.teams[i].players[j].name)
+                nba.teams[i].players[j].stats.forEach(element => {
+                    console.log(" PTS: " + element.PTS,"\n", "AST: "+ element.AST,"\n", "REB: "+element.REB,"\n","BLK: "+element.BLK,"\n", "STL: "+element.STL);
+                });
            }
 
         }
 
-
-
-        console.log(nba.teams.length)
     });
     
 
