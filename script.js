d35a71ea3418ef0b2121029328dc6e5170748db4
shebinho <33525543+shebinho@users.noreply.github.com>
2018-05-08 01:29:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,29 @@ $(document).ready(function () {
       
         // const filteredplayers = nba.filter(nba => nba.team[0].ATL.player)
         // $("<p>")
-        for(let i = 0 ; i < nba.teams.length ; i++){
-            console.log("Team Name: "+ nba.teams[i].name)
-            for(let j = 0; j < 5; j++) {
-                nba.teams[i].players[j].stats.forEach(element => {
-                    if(nba.teams[i].players[j].name === "Kent Bazemore"){
-                        console.log("Player Name: "+nba.teams[i].players[j].name,"\n", "Position: "+ nba.teams[i].players[j].position)
-                        console.log(" PTS: " + element.PTS,"\n", "AST: "+ element.AST,"\n", "REB: "+element.REB,"\n","BLK: "+element.BLK,"\n", "STL: "+element.STL);
-                    }
-                    
-                });
+        let saveButton = $("#saveButton");
+        $(saveButton).click(function(event){
+            event.preventDefault();
+
+            let playerName = $("#player-name").val();
+
+            for(let i = 0 ; i < nba.teams.length ; i++){
+                console.log("Team Name: "+ nba.teams[i].name)
+                for(let j = 0; j < 5; j++) {
+                    nba.teams[i].players[j].stats.forEach(element => {
+                        if(nba.teams[i].players[j].name === playerName){
+                            console.log("Player Name: "+nba.teams[i].players[j].name,"\n", "Position: "+ nba.teams[i].players[j].position)
+                            console.log(" PTS: " + element.PTS,"\n", "AST: "+ element.AST,"\n", "REB: "+element.REB,"\n","BLK: "+element.BLK,"\n", "STL: "+element.STL);
+                        }
+                        
+                    });
                 
-                    
-                
-                
+               }
+    
+            }
+
+        })
+   
                 // let filteredplayer = nba.teams.filter(element =>{
                 //     for(let i = 0; i < element.length; i++){
                 //         for(let j = 0; j < element.players.length; j++){
@@ -42,9 +51,6 @@ $(document).ready(function () {
                 // nba.teams[i].players[j].stats.forEach(element => {
                 //     console.log(" PTS: " + element.PTS,"\n", "AST: "+ element.AST,"\n", "REB: "+element.REB,"\n","BLK: "+element.BLK,"\n", "STL: "+element.STL);
                 // });
-           }
-
-        }
 
     });
     
