a5f4768db93bca2ef1a2f25db942f2edebcb6f16
shebinho <33525543+shebinho@users.noreply.github.com>
2018-05-08 01:06:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,40 @@ $(document).ready(function () {
         // $("<p>")
         for(let i = 0 ; i < nba.teams.length ; i++){
             console.log("Team Name: "+ nba.teams[i].name)
-            for(let j = 0; j < nba.teams[i].players.length; j++) {
-                console.log("Player Name: "+nba.teams[i].players[j].name)
+            for(let j = 0; j < 5; j++) {
                 nba.teams[i].players[j].stats.forEach(element => {
-                    console.log(" PTS: " + element.PTS,"\n", "AST: "+ element.AST,"\n", "REB: "+element.REB,"\n","BLK: "+element.BLK,"\n", "STL: "+element.STL);
+                    if(nba.teams[i].players[j].name === "Kent Bazemore"){
+                        console.log("Player Name: "+nba.teams[i].players[j].name,"\n", "Position: "+ nba.teams[i].players[j].position)
+                        console.log(" PTS: " + element.PTS,"\n", "AST: "+ element.AST,"\n", "REB: "+element.REB,"\n","BLK: "+element.BLK,"\n", "STL: "+element.STL);
+                    }
+                    
                 });
+                
+                    
+                
+                
+                // let filteredplayer = nba.teams.filter(element =>{
+                //     for(let i = 0; i < element.length; i++){
+                //         for(let j = 0; j < element.players.length; j++){
+                //             element.players[j].name === "Kent Bazemore";
+                //         }
+                //     }
+                // }) 
+
+                
+                // console.log(filteredplayer);
+
+                //  let nbal= nba.teams.name.filter(function(item) {
+                //     for (var key in nba) {
+                //         if(item[key] === "Atlanta Hawks");
+                //     }
+                //     return true;
+                //   });
+                //   console.log(nbal);
+
+                // nba.teams[i].players[j].stats.forEach(element => {
+                //     console.log(" PTS: " + element.PTS,"\n", "AST: "+ element.AST,"\n", "REB: "+element.REB,"\n","BLK: "+element.BLK,"\n", "STL: "+element.STL);
+                // });
            }
 
         }
